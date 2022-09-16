23828380fc721b30522494ac17573b03450c3455
samuel-walters <swalters@spartaglobal.com>
2022-09-17 00:34:37 +0100

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var mongo = require('mongodb');
-var server = express();
+const express = require('express');
+const mongo = require('mongodb');
+const server = express();
 
-var url = 'mongodb://localhost:27017/db-name'
+const url = 'mongodb://localhost:27017/db-name'
 
 server.use(express.static(__dirname + '/public'));
 
 server.listen(3000, () => {
-  console.log(`Server listening on port 3000`)
+  console.log(`Server listening on port 3000!`)
 });
