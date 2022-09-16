17b020999469ead4c61e2ee7cd8b84bc8f289335
samuel-walters <swalters@spartaglobal.com>
2022-09-16 12:56:16 +0100

    add basic server.js file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(3000, function() {
+  console.log('The server is ready and listening on port 3000');
+});
