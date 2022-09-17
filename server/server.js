3e5fcfb99f9f9bf57d0e9e56cba5f2385e772e4c
samuel-walters <swalters@spartaglobal.com>
2022-09-17 23:51:29 +0100

    Minor changes

--- server/server.js
+++ server/server.js
@@ -5,24 +5,50 @@ const bodyParser = require('body-parser');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/my-db");
 const nameSchema = new mongoose.Schema({
-  firstName: String,
-  lastNameName: String
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 const User = mongoose.model("User", nameSchema);
 
 const server = express();
-
+// tidies up the request object.
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.static(__dirname + '/public'));
+server.set("view engine", "ejs");
+// post
 
 server.post("/addname", (req, res) => {
-  var myData = new User(req.body);
-  myData.save()
-    .then(item => {
-      res.send("item saved to database");
-    })
-    .catch(err => {
-      res.status(400).send("unable to save to database");
-    });
+  const insertedData = new User(req.body);
+  insertedData.save(function (err) {
+    if (err) return console.error(err);
+    res.send("User successfully added to DB!");
+    console.log(req.body + "Saved to database.");
+  });
+});
+
+// get
+
+server.get("/hi", async (req, res) => {
+
+  const readData = await User.find({});
+  /*
+  try {
+    res.send(readData);
+  } catch (err) {
+    return console.error(err);
+  }
+  */
+  res.render("index.ejs", {userList: readData})
 });
 
 server.listen(3000, () => {
